8e903238034e6f1321fdbc5cc48ff71bd2d757f6
Adib Raed <adibraed1@gmail.com>
2022-11-18 12:16:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log(`listening in port ${process.env.PORT}`); // eslint-disable-line no-console
+  server.listen(process.env.PORT || 4000, () => {
+    console.log(`listening in port ${process.env.PORT || 4000}`); // eslint-disable-line no-console
   });
 });
